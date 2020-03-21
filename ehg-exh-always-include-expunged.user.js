c2c703cb114a348a9e701d566355bbab1960f314
dragontamer8740 <dragontamer8740@gmail.com>
2020-03-21 16:20:58 -0400

    cleaned up comments a little

--- ehg-exh-always-include-expunged.user.js
+++ ehg-exh-always-include-expunged.user.js
@@ -19,31 +19,39 @@
 // ==/UserScript==
 
 // add a convenient ID for future use (also avoids some overhead from multiple selectors)
-if(!document.getElementById("showAdvSearchLink"))
+if(document.querySelector('input[name="f_search"]')) // only if search bar exists on current page
 {
-  document.querySelector('a[onclick="toggle_advsearch_pane(this); return false"]').setAttribute("id", "showAdvSearchLink");
-}
-if(document.getElementById("advdiv").style.display == "none")
-{
-  // we use unsafeWindow to call page functions in Greasemonkey 4; otherwise, we don't prefix page function calls.
-  // first, try the greasemonkey 4 call. If that fails, try the normal page call. If both fail, we've failed.
-  try{ unsafeWindow.toggle_advsearch_pane(document.getElementById("showAdvSearchLink")); }
-  catch(e)
+  if(!document.getElementById("showAdvSearchLink"))
   {
-    console.log("1 couldn't run 'toggle_advsearch_pane()'");
-    // try un-prefixed version (non-GM4):
-  	try{ toggle_advsearch_pane(document.getElementById("showAdvSearchLink")); }
-    catch(e){console.log("(user script): couldn't find a way to run 'toggle_advsearch_pane()' from the source page. Probably a sandboxing problem I failed to account for. This is a bug! Please report it!")}
+    document.querySelector('a[onclick="toggle_advsearch_pane(this); return false"]').setAttribute("id", "showAdvSearchLink");
   }
-}
-function checkExpungedLocal()
-{
-  if(document.getElementById("advdiv").style.display != "none")
+  if(document.getElementById("advdiv").style.display == "none")
   {
+    // we use unsafeWindow to call page functions in Greasemonkey 4; otherwise,
+    // we don't prefix page function calls.
+    // First, try the greasemonkey 4 call. If that fails, try the normal
+    // call. If both fail, we can't keep going.
+    try {
+      unsafeWindow.toggle_advsearch_pane(document.getElementById("showAdvSearchLink"));
+    }
+    catch(e)
+    {
+      // try un-prefixed version (non-GM4):
+  	  try{
+        toggle_advsearch_pane(document.getElementById("showAdvSearchLink"));
+      }
+      catch(e){console.log("(user script): couldn't find a way to run 'toggle_advsearch_pane()' from the source page. Probably a sandboxing problem I failed to account for. This is a bug! Please report it!")}
+    }
+  }
+  function checkExpungedLocal()
+  {
+    if(document.getElementById("advdiv").style.display != "none")
+    {
       document.querySelector('input[name="f_sh"]').checked=true;
+    }
   }
+  checkExpungedLocal();
+  // store on the topmost level (window)
+  window.checkExpunged=checkExpungedLocal;
+  document.getElementById("showAdvSearchLink").addEventListener("click", window.checkExpunged);
 }
-checkExpungedLocal();
-// store on the topmost level (window)
-window.checkExpunged=checkExpungedLocal;
-document.getElementById("showAdvSearchLink").addEventListener("click", window.checkExpunged);
