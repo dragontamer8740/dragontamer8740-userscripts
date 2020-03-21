987a84fffddd174a8318f2e6210d8b8d4be3871c
dragontamer8740 <dragontamer8740@gmail.com>
2020-03-21 14:52:24 -0400

    Added another script for opening images automatically after they load in background tabs after a few seconds. Also handles automatically trying to reload pages if they give certain error messages instead of loading properly.

---
+++ ehg-exh-automatically-open-original-images-in-backgrounded-tabs.user.js
@@ -0,0 +1,73 @@
+// ==UserScript==
+// @name        Automatically open original images in background tabs
+// @namespace   dragontamer8740.xTabAutoOrig
+// @description Load original images if tabs are backgrounded after 3 seconds. Hooks off of my hotkey script, so it _must_ execute after it.
+// @version     1
+// @include     https://exhentai.org/s/*
+// @include     http://exhentai.org/s/*
+// @include     https://e-hentai.org/s/*
+// @include     http://e-hentai.org/s/*
+// @include     https://exhentai.org/g/*
+// @include     http://exhentai.org/g/*
+// @include     https://e-hentai.org/g/*
+// @include     http://e-hentai.org/g/*
+// @include     http://exhentai.org/*
+// @include     https://exhentai.org/*
+// @include     http://rule34.xxx/index.php*
+// @include     https://rule34.xxx/index.php*
+// @include     http://e621.net/post/show/*
+// @include     https://e621.net/post/show/*
+// @include     http://e621.net/posts/*
+// @include     https://e621.net/posts/*
+// @include     http://chan.sankakucomplex.com/post/show/*
+// @include     https://chan.sankakucomplex.com/post/show/*
+// @include     http://*.furaffinity.net/view/*/
+// @include     https://*.furaffinity.net/view/*/
+// @include     http://*.furaffinity.net/gallery/*/
+// @include     https://*.furaffinity.net/gallery/*/
+
+// @grant       unsafeWindow
+// ==/UserScript==
+// unsafeWindow allows us to call scripts exported by hotkey script.
+// we only allow things other than the basic ehg/s/ stuff so that we can turn off the script from the menu while anywhere on the site
+function doFocusCheckOpen() {
+  if(!document.hasFocus())
+  {
+    unsafeWindow.openImgHere();
+  }
+}
+
+// setInterval(doFocusCheck, 2000);
+var urlwoprot=window.location.href.replace(/(^\w+:|^)\/\//, '');
+if(urlwoprot.startsWith("exhentai.org/s/") | urlwoprot.startsWith("e-hentai.org/s/"))
+{
+  setTimeout(doFocusCheckOpen, 1500);
+}
+else if( window.location.origin.endsWith("rule34.xxx"))
+{
+  if(/\?s\=view|&s\=view/i.test(window.location.href) )
+  {
+    setTimeout(doFocusCheckOpen, 3000);
+  }
+}
+else if(urlwoprot.startsWith("e621.net/post/show/"))
+{
+  setTimeout(doFocusCheckOpen, 1500);
+}
+else if (urlwoprot.startsWith("chan.sankakucomplex.com"))
+{
+  setTimeout(doFocusCheckOpen, 1500);
+}
+else if (urlwoprot.startsWith("www.furaffinity.net/view/"))
+{
+  setTimeout(doFocusCheckOpen, 1500);
+}
+
+if(document.title=="503 Backend fetch failed")
+{
+    setTimeout(function(){ location.reload(); }, 2*1000);
+}
+else if (document.title=="I Just Don't Know What Went Wrong")
+{
+    setTimeout(function(){ location.reload(); }, 2*1000);
+}
