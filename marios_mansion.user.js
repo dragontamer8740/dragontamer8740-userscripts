7cf820855026319840e7272b3d77a574543cada3
dragontamer8740 <dragontamer8740@gmail.com>
2022-01-18 18:26:39 -0500

    evict luigi

---
+++ marios_mansion.user.js
@@ -0,0 +1,16 @@
+// ==UserScript==
+// @name        Mario's Mansion
+// @namespace   dragontamer8740.mariosMansion
+// @description Evict Luigi
+// @include     http://forums.e-hentai.org/index.php?showtopic=*
+// @include     https://forums.e-hentai.org/index.php?showtopic=*
+// @version     1
+// @grant       none
+// ==/UserScript==
+var posts=document.querySelectorAll(".postdetails .bigusername a[href$='index.php?showuser=5175192']");
+function findParentTable(elem) {
+  while(elem && elem.tagName.toUpperCase() !== 'TABLE')
+    elem=elem.parentElement;
+  return elem;
+}
+posts.forEach(p => findParentTable(p).setAttribute('style', 'display: none'));
