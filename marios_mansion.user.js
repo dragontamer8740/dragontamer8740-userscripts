22eedb538a89a0b8e59acda0c7a58b306bb87074
dragontamer8740 <dragontamer8740@gmail.com>
2022-01-19 12:54:28 -0500

    Mario can now evict more people, if necessary.

--- marios_mansion.user.js
+++ marios_mansion.user.js
@@ -2,17 +2,21 @@
 // @name        Mario's Mansion
 // @namespace   dragontamer8740.mariosMansion
 // @description Evict Luigi
-// @include     http://forums.e-hentai.org/index.php?*showtopic=*
-// @include     https://forums.e-hentai.org/index.php?*showtopic=*
-// @include     https://forums.e-hentai.org/index.php?*st=*
-// @include     https://forums.e-hentai.org/index.php?*st=*
+// @include     /^https?://forums\.e-hentai\.org/index\.php?.*showtopic=.*$/
+// @include     /^https?://forums\.e-hentai\.org/index\.php?.*st=.*$/
 // @version     1
 // @grant       none
 // ==/UserScript==
-var posts=document.querySelectorAll(".postdetails .bigusername a[href$='index.php?showuser=5175192']");
+// to add more users, add a comma after the first user ID in the list and then your additional ID.
+// example:   [ 5175192, 4544793 ];
+var userlist= [ 5175192 ];
 function findParentTable(elem) {
   while(elem && elem.tagName.toUpperCase() !== 'TABLE')
     elem=elem.parentElement;
   return elem;
 }
-posts.forEach(p => findParentTable(p).setAttribute('style', 'display: none'));
+function filterUser(id) {
+  var posts=document.querySelectorAll(".postdetails .bigusername a[href$='index.php?showuser=" + id + "']");
+  posts.forEach(p => findParentTable(p).setAttribute('style', 'display: none'));
+}
+userlist.forEach(u => filterUser(u));
